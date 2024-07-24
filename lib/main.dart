273f3dfa69de7488293697e59b4ef722e9b6f944
Andrew Chee <andrewcheewei@gmail.com>
2024-07-24 16:51:29 -0400

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -89,14 +87,12 @@ class _LoginState extends State<Login> {
                   child: ElevatedButton(
                     onPressed: () {
                       if (_formKey.currentState!.validate()) {
-                        if (emailController.text == "andrewcheewei@gmail.com" &&
+                        if (emailController.text == "username" &&
                             passwordController.text == "password") {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => HomePage(
-                                      email: emailController.text,
-                                    )),
+                                builder: (context) => Navigation()),
                           );
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
@@ -122,26 +118,48 @@ class _LoginState extends State<Login> {
   }
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key, required this.email});
-  final String email;
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Navigation(),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
   int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
       bottomNavigationBar: NavigationBar(
-        indicatorColor: Colors.amber[799],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.blue,
         selectedIndex: currentPageIndex,
         destinations: const <Widget>[
           NavigationDestination(
-            selectedIcon: Icon(Icons.search),
-            icon: Icon(Icons.search),
-            label: 'Search',
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.fitness_center),
-            icon: Icon(Icons.fitness_center_outlined),
+            icon: Icon(Icons.fitness_center),
             label: 'Train',
           ),
           NavigationDestination(
@@ -149,32 +167,111 @@ class HomePage extends StatelessWidget {
             label: 'Programs',
           ),
           NavigationDestination(
-            selectedIcon: Icon(Icons.settings),
             icon: Icon(Icons.settings),
             label: 'Settings',
-          ),
+          )
         ],
       ),
       body: <Widget>[
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Find new program'),
+        // Home page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(
+              child: Text(
+                'Home page',
+                style: theme.textTheme.titleLarge,
+              ),
+            ),
+          ),
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Train'),
+
+        // Train page
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.fitness_center),
+                  title: Text('Program 1'),
+                  subtitle: Text('Program 1'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.fitness_center),
+                  title: Text('Program 2'),
+                  subtitle: Text('Program 2'),
+                ),
+              ),
+            ],
+          ),
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Programs'),
+
+        // Programs page
+        ListView.builder(
+          reverse: true,
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'bar',
+                    style: theme.textTheme.bodyLarge!
+                        .copyWith(color: theme.colorScheme.onPrimary),
+                  ),
+                ),
+              );
+            }
+            return Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.primary,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  'foo',
+                  style: theme.textTheme.bodyLarge!
+                      .copyWith(color: theme.colorScheme.onPrimary),
+                ),
+              ),
+            );
+          },
         ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text('Settings'),
+        // Settings
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Setting 1'),
+                  subtitle: Text('Setting 1'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Setting 2'),
+                  subtitle: Text('Setting 2'),
+                ),
+              ),
+            ],
+          ),
         ),
       ][currentPageIndex],
     );
